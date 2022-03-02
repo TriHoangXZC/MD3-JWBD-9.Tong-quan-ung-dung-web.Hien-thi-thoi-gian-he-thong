e8a9dd17fd1913990f38d0f6240d595f86994392
TriHoangZXC <hoanghuutri97@gmail.com>
2022-03-02 10:03:13 +0700

    add [TH] Hien thi thoi gian he thong file

---
+++ src/main/java/com/codegym/ServerTimeServlet.java
@@ -0,0 +1,26 @@
+package com.codegym;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", value = "/server-time")
+public class ServerTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        printWriter.println("<h1>Hello world</h1>");
+        Date date = new Date();
+        printWriter.println("<h1>" + date + "</h1>");
+        printWriter.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
